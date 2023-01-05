1596b92ede222b6a5455c172fc7328e880405c8a
hayhyang <hayhyang@gmail.com>
2023-01-05 09:02:29 +0900

    vuex: state, mutations, actions, getters

---
+++ src/views/CartView.vue
@@ -0,0 +1,25 @@
+<template>
+  <button @click='decrement'> - </button>
+  {{count}}
+  <button @click='increment'> + </button>
+</template>
+
+<script>
+export default {
+  component: {},
+  data () {
+    return {
+      sampleData: ''
+    }
+  },
+  setup () {},
+  created () {},
+  mounted () {},
+  unmounted () {},
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/CountView.vue
@@ -0,0 +1,54 @@
+<template>
+  <button @click='decrement'> decrement </button>
+  {{test2}} {{count}} {{oddOrEven}}
+  <button @click='increment'> increment </button>
+  <button @click='incrementAsync'> incrementAsync </button>
+  <button @click='actionA'> actionA </button>
+  <button @click='actionB'> actionB </button>
+</template>
+
+<script>
+export default {
+  component: {},
+  data () {
+    return {
+      sampleData: ''
+    }
+  },
+  setup () {},
+  created () {},
+  mounted () {
+    console.log('store', this.$store)
+  },
+  unmounted () {},
+  methods: {
+    increment() {
+      this.$store.commit({ type: 'increment', amount: 4 })
+    },
+    decrement() {
+      this.$store.commit({ type: 'decrement', amount: 3 })
+    },
+    incrementAsync() {
+      this.$store.dispatch('incrementAsync', { amount: 10 })
+    },
+    actionA() {
+      this.$store.dispatch('actionA', { amount: 200 }).then(() => console.log('commit actionA'))
+    },
+    actionB() {
+      this.$store.dispatch('actionB')
+    }
+  },
+  computed: {
+    count () {
+      return this.$store.state.count.count
+    },
+    oddOrEven () {
+      return this.$store.getters.oddOrEven
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
